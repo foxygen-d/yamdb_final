1249fae585f947c19311a3514a23eb7d7d55f89f
Anastasia Domnina <foxygen.d@yandex.ru>
2022-08-18 16:42:01 +0300

    ready

--- api_yamdb/reviews/utils.py
+++ api_yamdb/reviews/utils.py
@@ -12,9 +12,8 @@ def get_csv_data(source) -> List[Dict]:
         os.path.join(base_dir, f'static/data/{source}.csv'), 'r'
     ) as file:
         csv_dict = csv.DictReader(file)
-    return [
-            {attr: row.get(attr) for attr in row} for row in csv_dict
-        ]
+    return [{attr: row.get(attr) for attr in row}
+            for row in csv_dict]
 
 
 def set_by_id(payload: Dict, field: AnyStr) -> None:
